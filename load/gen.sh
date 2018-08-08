c8a1cd95c40777193a1b42fc19a695db2aa509ef
CK Tan <cktan@vitessedata.com>
2018-08-07 17:08:41 -0700

    fix

--- load/gen.sh
+++ load/gen.sh
@@ -74,8 +74,8 @@ wait
 
 # push tbl files to s3://vitessedata/tpch/*
 aws s3 rm s3://vitessedata/tpch/ --recursive
-ssh sdw1 'cd /data4/dbgen && rm -f list* && ls -1 *tbl* > list && spit -n 8 list list' & 
-ssh sdw2 'cd /data4/dbgen && rm -f list* && ls -1 *tbl* > list && spit -n 8 list list' & 
+ssh sdw1 'cd /data4/dbgen && rm -f list* && ls -1 *tbl* > list && split -n 8 list list' & 
+ssh sdw2 'cd /data4/dbgen && rm -f list* && ls -1 *tbl* > list && split -n 8 list list' & 
 wait
 
 ssh sdw1 'cd /data4/dbgen && 
