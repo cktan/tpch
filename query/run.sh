9acd6f530815435ec9d1149000180cf7110ed71a
CK Tan <cktan@vitessedata.com>
2018-08-08 23:20:56 -0700

    echo

--- query/run.sh
+++ query/run.sh
@@ -1,5 +1,9 @@
+echo "Make sure you run ANALYZE on the tpch database."
+
 echo vitesse.enable=0
 psql tpch -ef run.sql -v exx=0 >& out0
+
 echo vitesse.enable=1
 psql tpch -ef run.sql -v exx=1 >& out1
+
 python parse.py out0 out1
