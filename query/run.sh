fb67632c7813120b3b7c20422ed844e8eec5202b
CK Tan <cktan@vitessedata.com>
2018-08-08 17:39:15 -0700

    fix

--- query/run.sh
+++ query/run.sh
@@ -1,5 +1,5 @@
 echo vitesse.enable=0
-psql tpch -ef run0.sql -v exx=0 > out0
+psql tpch -ef run0.sql -v exx=0 >& out0
 echo vitesse.enable=1
-psql tpch -ef run1.sql -v exx=1 > out1
+psql tpch -ef run1.sql -v exx=1 >& out1
 python parse.py out0 out1
--- query/run.sql
+++ query/run.sql
@@ -31,49 +31,49 @@ set statement_mem=2000MB;
 \timing on
 
 \echo query 0
-create table out0 as select * from q0
+create temp table out0 as select * from q0
 \echo query 1
-create table out1 as select * from q1
+create temp table out1 as select * from q1
 \echo query 2
-create table out2 as select * from q2
+create temp table out2 as select * from q2
 \echo query 3
-create table out3 as select * from q3
+create temp table out3 as select * from q3
 \echo query 4
-create table out4 as select * from q4
+create temp table out4 as select * from q4
 \echo query 5
-create table out5 as select * from q5
+create temp table out5 as select * from q5
 \echo query 6
-create table out6 as select * from q6
+create temp table out6 as select * from q6
 \echo query 7
-create table out7 as select * from q7
+create temp table out7 as select * from q7
 \echo query 8
-create table out8 as select * from q8
+create temp table out8 as select * from q8
 \echo query 9
-create table out9 as select * from q9
+create temp table out9 as select * from q9
 \echo query 10
-create table out10 as select * from q10
+create temp table out10 as select * from q10
 \echo query 11
-create table out11 as select * from q11
+create temp table out11 as select * from q11
 \echo query 12
-create table out12 as select * from q12
+create temp table out12 as select * from q12
 \echo query 13
-create table out13 as select * from q13
+create temp table out13 as select * from q13
 \echo query 14
-create table out14 as select * from q14
+create temp table out14 as select * from q14
 \echo query 15
-create table out15 as select * from q15
+create temp table out15 as select * from q15
 \echo query 16
-create table out16 as select * from q16
+create temp table out16 as select * from q16
 \echo query 17
-create table out17 as select * from q17
+create temp table out17 as select * from q17
 \echo query 18
-create table out18 as select * from q18
+create temp table out18 as select * from q18
 \echo query 19
-create table out19 as select * from q19
+create temp table out19 as select * from q19
 \echo query 20
-create table out20 as select * from q20
+create temp table out20 as select * from q20
 \echo query 21
-create table out21 as select * from q21
+create temp table out21 as select * from q21
 \echo query 22
-create table out22 as select * from q22
+create temp table out22 as select * from q22
 
